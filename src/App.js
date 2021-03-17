2e77a6a93ae12a817478da3b20801f33cf6c71e8
Mazharul-Haque <mshagor009@gmail.com>
2021-03-17 17:18:32 +0600

    Name,email,photo display in UI

--- src/App.js
+++ src/App.js
@@ -3,35 +3,70 @@ import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
-}else {
+} else {
   firebase.app(); // if already initialized, use that one
 }
 
 function App() {
+  const [user, setUser] = useState({})
   var provider = new firebase.auth.GoogleAuthProvider();
-  const handleGoogleSignIn = () =>{
+  const handleGoogleSignIn = () => {
     firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    var credential = result.credential;
-    var token = credential.accessToken;
-    var user = result.user;
-    console.log(user);
-  }).catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    console.log(email,errorCode,errorMessage);
-  });
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(email, errorCode, errorMessage);
+      });
 
   }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn} className="btn btn-success mt-5 text-center" >SignIn by Google</button>
+
+      <div class="row m-5" >
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+              <h4>Name : <small>{user.displayName}</small></h4>
+              <h5>Email : <small>{user.email}</small></h5> <br />
+              <img src={user.photoURL} alt="" /> <br />
+              <button onClick={handleGoogleSignIn} className="btn btn-success mt-5 text-center" >SignIn by Google</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
